86315a785817f368eebc49e644df81321d5d8f08
joeflack4 <joeflack4@gmail.com>
2017-08-25 16:55:52 -0400

    Added resources route for datalab.

--- pma_api/api_1_0/__init__.py
+++ pma_api/api_1_0/__init__.py
@@ -33,3 +33,65 @@ def show_version():
         'version': __version__
     }
     return jsonify(response)
+
+
+# TODO: Handle null cases.
+@api.route('/datalab/data')
+def get_datalab_data():
+    """Get the correct slice of datalab data."""
+    if not request.args:
+        json_obj = DatalabData.get_all_datalab_data()
+    elif 'survey' not in request.args or 'indicator' not in request.args \
+            or 'characteristicGroup' not in request.args:
+        return 'InvalidArgsError: This endpoint requires the following 3 ' \
+               'parameters: \n* survey\n* indicator\n* characteristicGroup'
+    else:
+        survey = request.args.get('survey', None)
+        indicator = request.args.get('indicator', None)
+        char_grp = request.args.get('characteristicGroup', None)
+        json_obj = DatalabData.get_filtered_datalab_data(survey, indicator,
+                                                         char_grp)
+
+    return jsonify(json_obj)
+
+
+@api.route('/datalab/combos')
+def get_datalab_combos():
+    """Get datalab combos."""
+    # TODO: Account for all combinations of request args or lack thereof.
+    # TODO: Add logic to sort by arguments. If you have indicator, go to
+    # this method.
+
+    if 'survey' not in request.args and 'indicator' not in request.args \
+            and 'characteristicGroup' not in request.args:
+        return 'InvalidArgsError: This endpoint requires 1-2 of 3 ' \
+               'parameters: \n* survey\n* indicator\n* characteristicGroup'
+
+    # return jsonify(DatalabData.related_models_from_single_model_data(
+    #     request.args))
+    return jsonify(DatalabData.get_combos(request.args))
+
+
+@api.route('/datalab/init')
+def get_datalab_init():
+    """Get datalab combos."""
+    return jsonify(DatalabData.datalab_init())
+
+@api.route('/datalab')
+def get_datalab():
+    return get_datalab_resources()
+
+@api.route('/datalab/resources')
+def get_datalab_resources():
+    """Get datalab routes."""
+    resource_endpoints = (
+        ('init', 'api.get_datalab_init'),
+        ('combos', 'api.get_datalab_combos'),
+        ('data', 'api.get_datalab_data')
+    )
+    return jsonify({
+        'resources': [{
+            'name': name,
+            'resource': url_for(route, _external=True)}
+            for name, route in resource_endpoints]
+    })
