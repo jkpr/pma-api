32d71afc979346f21a33a3e30dbefd7e3144bb1c
joeflack4 <joeflack4@gmail.com>
2017-09-01 12:53:54 -0400

    Added util HiddenPrints. Refactorings. WIP.

---
+++ test/utils/__init__.py
@@ -0,0 +1,21 @@
+"""Utils."""
+from .doctest_unittest_runner import doctest_unittest_runner
+
+
+class HiddenPrints:
+    """Allows code to run in a closure without printing to stdout.
+
+    Example:
+        with HiddenPrints():
+            print('This will not appear in stdout.')
+    """
+
+    def __enter__(self):
+        import sys;
+        import os
+        self._original_stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        import sys
+        sys.stdout = self._original_stdout
